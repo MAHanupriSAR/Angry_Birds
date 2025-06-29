5f85d91a2bef40520323aea99175cc9d44c15330
MAHanupriSAR <codingat4@gmail.com>
2025-06-29 19:11:06 +0530

    delete

--- core/src/main/java/angry_bird/Levels/Level1.java
+++
@@ -1,8 +0,0 @@
-package angry_bird.Levels;
-
-public class Level1 extends Level {
-    public Level1() {
-        setTiledMap("level/level1/level1.tmx");
-        background.setImage("level/bg2.png");
-    }
-}
--- core/src/main/java/angry_bird/Levels/Level2.java
+++
@@ -1,8 +0,0 @@
-package angry_bird.Levels;
-
-public class Level2 extends Level{
-    public Level2(){
-        setTiledMap("level/level2/level2.tmx");
-        background.setImage("level/bg2.png");
-    }
-}
